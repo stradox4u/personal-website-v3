e5e539463d2a7182546346258afeea25185fb83c
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2021-05-18 12:14:19 +0100

    ditto last commit

--- src/components/MyWebPortfolio.vue
+++ src/components/MyWebPortfolio.vue
@@ -2,7 +2,7 @@
     <div class="w-full flex flex-col gap-8 mx-auto">
         <h1 class="font-montserrat text-2xl font-bold text-center">Web Projects</h1>
         <div class="w-full flex flex-col gap-8 bg-gray-200 p-4 rounded-md shadow-md">
-            <ul class="grid sm:grid-cols-3 grid-cols-1 gap-4">
+            <ul class="grid sm:grid-cols-3 grid-cols-1 gap-4 mb-4">
                 <project-card v-for="item in portfolioItems" :key="item.name"
                     :item="item" :class="{ 'transform scale-105 shadow-lg': item.name.includes(activeApp)}">
                 </project-card>
